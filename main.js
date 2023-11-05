81f07c449b8c5dddf596cf8a50e8ddc6524281d1
navidiuming <navid.abolfathi@gmail.com>
2023-11-05 21:59:44 +0330

    eddit display position

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const displayResult = document.getElementById("displayResult");
 const btn_buttouns = document.querySelectorAll(".padBtn");
 
-
 btn_buttouns.forEach(item => {
     item.addEventListener('click', event => {
-        console.log(item);
+      clickOnNumbers(item);
+
+        
     })
   })
+
+
+  function clickOnNumbers(el){
+    if(el.classList.contains("digitBtn")){
+        let numberInDisplayBeforeClick;
+        let enterdNumber;
+
+        numberInDisplayBeforeClick = displayResult.innerHTML;
+        enterdNumber = el.innerHTML;
+        enterdNumber = checkIsNumberDecimal(numberInDisplayBeforeClick) && enterdNumber == "." ? "" :enterdNumber;
+        numberInDisplayBeforeClick = enterdNumber!="." && numberInDisplayBeforeClick=="0" ? "" : numberInDisplayBeforeClick;
+
+        displayResult.innerHTML = numberInDisplayBeforeClick+enterdNumber;
+
+    }
+  }
+
+  function checkIsNumberDecimal(num){
+    return num.includes(".");
+  }
