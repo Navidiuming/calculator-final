9ad2dfbde97a3e3a0558feb3de198f1f417a0faa
navidiuming <navid.abolfathi@gmail.com>
2024-01-05 12:57:15 +0330

    add 3 theme to calculator

--- main.js
+++ main.js
@@ -1,30 +1,111 @@
 const displayResult = document.getElementById("displayResult");
 const btn_buttouns = document.querySelectorAll(".padBtn");
+const btn_themeChanger = document.getElementById("btn_themeChanger");
+const div_mainContainer = document.querySelector(".mainContainer")
 
-btn_buttouns.forEach(item => {
-    item.addEventListener('click', event => {
-      clickOnNumbers(item);
+for (let i = 0; i < btn_buttouns.length; i++) {
+  btn_buttouns[i].addEventListener("click", clickOnBtnPad);
+}
 
-        
-    })
-  })
+function clickOnBtnPad() {
+  console.log(this.classList.contains("digitBtn"));
+  if (this.classList.contains("digitBtn")) {
+    let clickedBtn = this.innerHTML;
+    let displayResultBeforeClick = displayResult.innerHTML;
+    displayResultBeforeClick = displayResult.innerHTML == "0" && this.innerHTML != "." ? "" : displayResult.innerHTML;
+    clickedBtn = displayResult.innerHTML.includes(".") && this.innerHTML == "." ? "" : this.innerHTML;
+    displayResult.innerHTML = displayResultBeforeClick + clickedBtn;
+  }
 
+  if (this.classList.contains("mathBtn")) {
 
-  function clickOnNumbers(el){
-    if(el.classList.contains("digitBtn")){
-        let numberInDisplayBeforeClick;
-        let enterdNumber;
+    // console.log(this.getAttribute("id"));
 
-        numberInDisplayBeforeClick = displayResult.innerHTML;
-        enterdNumber = el.innerHTML;
-        enterdNumber = checkIsNumberDecimal(numberInDisplayBeforeClick) && enterdNumber == "." ? "" :enterdNumber;
-        numberInDisplayBeforeClick = enterdNumber!="." && numberInDisplayBeforeClick=="0" ? "" : numberInDisplayBeforeClick;
-
-        displayResult.innerHTML = numberInDisplayBeforeClick+enterdNumber;
-
+    let displayResultBeforeClick = displayResult.innerHTML;
+    switch (this.getAttribute("id")) {
+      case "divideRemaining":
+        console.log("%");
+        break;
+      case "clear":
+        console.log("clear");
+        break;
+      case "delet":
+        console.log("delet");
+        break;
+      case "divide":
+        console.log("/");
+        break;
+      case "root":
+        console.log("root");
+        break;
+      case "multiply":
+        console.log("*");
+        break;
+      case "power":
+        console.log("power");
+        break;
+      case "minus":
+        console.log("-");
+        break;
+      case "power3":
+        console.log("3");
+        break;
+      case "plus":
+        console.log("+");
+        break;
+      case "reverse":
+        console.log("1/x");
+        break;
+      case "mark":
+        console.log("-/+");
+        break;
+      case "equal":
+        console.log("=");
+        break;
+      default:
+        console.log("not found");
+        break;
     }
-  }
 
-  function checkIsNumberDecimal(num){
-    return num.includes(".");
   }
+}
+
+// change theme
+const themes = {
+  1 : "greenTheme",
+  2 : "blueTheme",
+  3 : "myTheme"
+}
+
+let themeCounter = 1;
+function changeTheme(){
+  console.log(themeCounter);
+  console.log(themes[themeCounter]);
+  themeCounter++;
+  if(themeCounter > 4 ){
+    themeCounter = 1;
+  }
+  switch(themeCounter){
+    case 1:
+      div_mainContainer.classList.add(themes[1]);
+      div_mainContainer.classList.remove(themes[2]);
+      div_mainContainer.classList.remove(themes[3]);
+      break;
+    case 2:
+      div_mainContainer.classList.add(themes[2]);
+      div_mainContainer.classList.remove(themes[1]);
+      div_mainContainer.classList.remove(themes[3]);
+      break;
+    case 3:
+      div_mainContainer.classList.add(themes[3]);
+      div_mainContainer.classList.remove(themes[2]);
+      div_mainContainer.classList.remove(themes[1]);
+      break;
+    case 4:
+      div_mainContainer.classList.remove(themes[2]);
+      div_mainContainer.classList.remove(themes[3]);
+      div_mainContainer.classList.remove(themes[1]);
+      break;
+  }
+}
+btn_themeChanger.addEventListener('click' , changeTheme);
