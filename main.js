f70d8a190f91f3c151e90cee63f678898802917c
navidiuming <navid.abolfathi@gmail.com>
2024-01-20 16:23:09 +0330

    add operation sign and power and root

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const displayResult = document.getElementById("displayResult");
 const btn_buttouns = document.querySelectorAll(".padBtn");
 const btn_themeChanger = document.getElementById("btn_themeChanger");
-const div_mainContainer = document.querySelector(".mainContainer")
-
+const div_mainContainer = document.querySelector(".mainContainer");
+let isOperationAdded = false;
+let isNumberAfterOperationAdded = false;
+let numberAfterOperation = "";
+let currentOperationSign = "";
+let isResultBtnPushed = false;
 for (let i = 0; i < btn_buttouns.length; i++) {
   btn_buttouns[i].addEventListener("click", clickOnBtnPad);
 }
@@ -10,20 +14,34 @@ for (let i = 0; i < btn_buttouns.length; i++) {
 function clickOnBtnPad() {
   console.log(this.classList.contains("digitBtn"));
   if (this.classList.contains("digitBtn")) {
-    let clickedBtn = this.innerHTML;
-    let displayResultBeforeClick = displayResult.innerHTML;
-    displayResultBeforeClick = displayResult.innerHTML == "0" && this.innerHTML != "." ? "" : displayResult.innerHTML;
-    clickedBtn = displayResult.innerHTML.includes(".") && this.innerHTML == "." ? "" : this.innerHTML;
-    displayResult.innerHTML = displayResultBeforeClick + clickedBtn;
+    if (isResultBtnPushed == true) {
+      displayResult.innerHTML = "0";
+      isResultBtnPushed = false;
+    }
+    // let clickedBtn = this.innerHTML;
+
+    // let displayResultBeforeClick = displayResult.innerHTML;
+    // displayResultBeforeClick = displayResult.innerHTML == "0" && this.innerHTML != "." ? "" : displayResult.innerHTML;
+    // clickedBtn = displayResult.innerHTML.includes(".") && this.innerHTML == "." ? "" : this.innerHTML;
+    // displayResult.innerHTML = displayResultBeforeClick + clickedBtn;
+    if (isOperationAdded == true) {
+      isNumberAfterOperationAdded = true;
+    }
+    checkDecimal(isNumberAfterOperationAdded, displayResult, this.innerHTML);
+
   }
 
   if (this.classList.contains("mathBtn")) {
-
-    // console.log(this.getAttribute("id"));
-
-    let displayResultBeforeClick = displayResult.innerHTML;
+    let lastResult = displayResult.innerHTML;
+    console.log(lastResult);
     switch (this.getAttribute("id")) {
       case "divideRemaining":
+        if (isOperationAdded && isNumberAfterOperationAdded == false) {
+          console.log("do nothing");
+        } else {
+          let result = displayResult.innerHTML + "/100";
+          displayResult.innerHTML = eval(result);
+        }
         console.log("%");
         break;
       case "clear":
@@ -34,24 +52,75 @@ function clickOnBtnPad() {
         break;
       case "divide":
         console.log("/");
+        mainOperatorCompute("/", lastResult);
         break;
       case "root":
         console.log("root");
+        if (isOperationAdded && isNumberAfterOperationAdded == false) {
+          console.log("do nothing");
+        } else if (isOperationAdded == false) {
+          let result = "Math.sqrt(" + displayResult.innerHTML + ")";
+          displayResult.innerHTML = eval(result);
+        } else if (isNumberAfterOperationAdded) {
+          let index = displayResult.innerHTML.indexOf(currentOperationSign);
+          let partOne = displayResult.innerHTML.slice(0, index);
+          let partTwo = displayResult.innerHTML.slice(index + 1);
+          let result = partOne + currentOperationSign + "Math.sqrt(" + partTwo + ")";
+          displayResult.innerHTML = eval(result);
+        }
         break;
       case "multiply":
         console.log("*");
+        mainOperatorCompute("*", lastResult);
         break;
       case "power":
         console.log("power");
+        if (isOperationAdded && isNumberAfterOperationAdded == false) {
+          console.log("do nothing");
+        } else if (isOperationAdded == false) {
+          let result = "Math.pow(" + displayResult.innerHTML + ",2)";
+          displayResult.innerHTML = eval(result);
+        } else if (isNumberAfterOperationAdded) {
+          let index = displayResult.innerHTML.indexOf(currentOperationSign);
+          let partOne = displayResult.innerHTML.slice(0, index);
+          let partTwo = displayResult.innerHTML.slice(index + 1);
+          let result = partOne + currentOperationSign + "Math.pow(" + partTwo + ",2)";
+          displayResult.innerHTML = eval(result);
+        }
         break;
       case "minus":
         console.log("-");
+        // if(isOperationAdded == false && isNumberAfterOperationAdded == false ){
+        //   displayResult.innerHTML = lastResult + " -";
+        //   isOperationAdded = true;
+        // }else if(isOperationAdded == true && isNumberAfterOperationAdded == false){
+        //   displayResult.innerHTML = lastResult.slice(0,-1) + " -";
+        // }else if(isOperationAdded && isNumberAfterOperationAdded){
+        //   let result = eval(lastResult);
+        //   displayResult.innerHTML = result + " -";
+        //   isOperationAdded = true;
+        //   isNumberAfterOperationAdded = false;
+        // }
+        mainOperatorCompute("-", lastResult);
+
         break;
       case "power3":
         console.log("3");
+        if (isOperationAdded && isNumberAfterOperationAdded == false) {
+          console.log("do nothing");
+        } else if (isOperationAdded == false) {
+          let result = "Math.pow(" + displayResult.innerHTML + ",3)";
+          displayResult.innerHTML = eval(result);
+        } else if (isNumberAfterOperationAdded) {
+          let index = displayResult.innerHTML.indexOf(currentOperationSign);
+          let partOne = displayResult.innerHTML.slice(0, index);
+          let partTwo = displayResult.innerHTML.slice(index + 1);
+          let result = partOne + currentOperationSign + "Math.pow(" + partTwo + ",3)";
+          displayResult.innerHTML = eval(result);
+        }
         break;
       case "plus":
-        console.log("+");
+        mainOperatorCompute("+", lastResult);
         break;
       case "reverse":
         console.log("1/x");
@@ -61,6 +130,7 @@ function clickOnBtnPad() {
         break;
       case "equal":
         console.log("=");
+        mainOperatorCompute("=", lastResult);
         break;
       default:
         console.log("not found");
@@ -69,23 +139,99 @@ function clickOnBtnPad() {
 
   }
 }
+function checkDecimal(checkCondition, displayResult, el) {
+  clickedBtn = el;
+  if (checkCondition == false) {
+    let displayResultBeforeClick = displayResult.innerHTML;
+    displayResultBeforeClick = displayResult.innerHTML == "0" && el != "." ? "" : displayResult.innerHTML;
+    clickedBtn = displayResult.innerHTML.includes(".") && el == "." ? "" : el;
+    displayResult.innerHTML = displayResultBeforeClick + clickedBtn;
+  } else {
+    numberAfterOperation = numberAfterOperation.includes(".") && el == "." ? numberAfterOperation : numberAfterOperation + el;
+    clickedBtn = numberAfterOperation.includes(".") && el == "." ? "" : el;
+    displayResult.innerHTML = displayResult.innerHTML + el;
+  }
+}
+function mainOperatorCompute(operationSign, lastResult) {
+  if (operationSign == "=") {
+    if (isNumberAfterOperationAdded == true) {
+      let result = eval(lastResult);
+      displayResult.innerHTML = result;
+      isOperationAdded = false;
+      isNumberAfterOperationAdded = false;
+      numberAfterOperation = "";
+      isResultBtnPushed = true;
+      currentOperationSign = "";
+    }
 
+  } else {
+    if (isOperationAdded == false && isNumberAfterOperationAdded == false) {
+      currentOperationSign = operationSign;
+      displayResult.innerHTML = lastResult + operationSign;
+      isOperationAdded = true;
+    } else if (isOperationAdded == true && isNumberAfterOperationAdded == false) {
+      currentOperationSign = operationSign;
+      displayResult.innerHTML = lastResult.slice(0, -1) + operationSign;
+    } else if (isOperationAdded && isNumberAfterOperationAdded) {
+      currentOperationSign = operationSign;
+      let result = eval(lastResult);
+      displayResult.innerHTML = result + operationSign;
+      isOperationAdded = true;
+      isNumberAfterOperationAdded = false;
+      numberAfterOperation = "";
+    }
+    isResultBtnPushed = false;
+  }
+}
+
+function otheroperatorBtn(operationSign) {
+  switch (operationSign) {
+    case "%":
+      console.log("%");
+      break;
+    case "clear":
+      console.log("clear");
+      break;
+    case "delet":
+      console.log("delet");
+      break;
+    case "root":
+      console.log("root");
+      break;
+    case "power":
+      console.log("power");
+      break;
+    case "power3":
+      console.log("3");
+      break;
+    case "reverse":
+      console.log("1/x");
+      break;
+    case "mark":
+      console.log("-/+");
+      break;
+    default:
+      console.log("not found");
+      break;
+  }
+
+}
 // change theme
 const themes = {
-  1 : "greenTheme",
-  2 : "blueTheme",
-  3 : "myTheme"
+  1: "greenTheme",
+  2: "blueTheme",
+  3: "myTheme"
 }
 
 let themeCounter = 1;
-function changeTheme(){
+function changeTheme() {
   console.log(themeCounter);
   console.log(themes[themeCounter]);
   themeCounter++;
-  if(themeCounter > 4 ){
+  if (themeCounter > 4) {
     themeCounter = 1;
   }
-  switch(themeCounter){
+  switch (themeCounter) {
     case 1:
       div_mainContainer.classList.add(themes[1]);
       div_mainContainer.classList.remove(themes[2]);
@@ -108,4 +254,4 @@ function changeTheme(){
       break;
   }
 }
-btn_themeChanger.addEventListener('click' , changeTheme);
+btn_themeChanger.addEventListener('click', changeTheme);
